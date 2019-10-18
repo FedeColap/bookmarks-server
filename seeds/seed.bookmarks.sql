588edf7885fd001d9d40f99d848ebf6db49be15a
Federica Colapaoli <federica.colapaoli@coresecure.com>
2019-10-18 11:23:41 -0400

    populated first database

---
+++ seeds/seed.bookmarks.sql
@@ -0,0 +1,10 @@
+INSERT INTO bookmarks (title, url, description, rating)
+VALUES
+('Etsy', 'https://www.etsy.com/', 'sito di opere fai da te', '5'),
+('Twitter', 'https://twitter.com/login?redirect_after_login=%2Fhome', 'mai usato prima', '4'),
+('Google News', 'https://news.google.com/?hl=en-US&gl=US&ceid=US:en', 'notizie google', '3'),
+('OVS', 'https://www.ovsfashion.com/', 'negozio di abbigliamento', '2'),
+('Youtube', 'https://www.youtube.com/', 'dove trovi i video', '5'),
+('Google', 'https://www.google.com/', 'sa ogni cosa, o quasi', '3'),
+('Il Corriere della Sera', 'https://www.corriere.it/', 'notizie liberali', '3'),
+('Chi Chi London', 'https://www.chichiclothing.com/', 'magari comprassi un vestito a dicembre... ma no', '3');
--- test/app.spec.js
+++ test/app.spec.js
@@ -5,6 +5,7 @@ describe('App', () => {
     it('GET / responds with 200 containing "Fede, Speranza e Carita"', () => {
         return supertest(app)
                             .get('/')
+                            .set('Authorization', `Bearer ${process.env.API_TOKEN}`)
                             .expect(200, 'Fede, Speranza e Carita')
     })
 })
